0647f62146c38a27809d286dc75fc829a591b135
Dawid Dylus <36481644+DawidDylus@users.noreply.github.com>
2021-07-22 13:58:21 +0200

    Adding base frontend layout.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,26 +1,129 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button, Radio } from "antd";
-import { getAllStudents } from "./client"
+import { getAllStudents } from "./client";
+import React, { useState, useEffect } from 'react';
+import { Layout, Menu, Breadcrumb, Table, Spin, Empty } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+  LoadingOutlined,
+} from '@ant-design/icons';
+
+
 
 function App() {
 
-getAllStudents()
-.then(res => res.json())
-.then(console.log);
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+  const columns = [
+
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id,'
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Gender',
+      dataIndex: 'gender',
+      key: 'gender',
+    }
+
+  ];
+
+
+  const [students, setStudents] = useState([]);
+
+  const { Header, Content, Footer, Sider } = Layout;
+  const { SubMenu } = Menu;
+  const [collapsed, setCollapsed] = useState(false);
+
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    fetchStudents();
+  },
+    []
+  )
+
+  const fetchStudents = () => {
+    getAllStudents()
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setStudents(data);
+        setFetching(false);
+      });
+  }
+
+  const RenderStudents = () => {
+    if (fetching) {
+      return <Spin indicator={antIcon} />
+    }
+
+    if (students.length <= 0) {
+      return <Empty />;
+    }
+    return <Table
+      dataSource={students}
+      columns={columns}
+      bordered
+      title={() => "Students"}
+      pagination={{ pageSize: 50 }}
+      scroll={{ y: 240 }}
+      rowKey={(student) => student.id} />
+  }
 
   return (
-    <div className="App">
-      <Button type={"primary"}>Hello</Button>
-      <br/>
-
-      <Radio.Group value="large" >
-          <Radio.Button value="large">Large</Radio.Button>
-          <Radio.Button value="default">Default</Radio.Button>
-          <Radio.Button value="small">Small</Radio.Button>
-        </Radio.Group>
-
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <RenderStudents />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Made by Dawid Dylus</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
