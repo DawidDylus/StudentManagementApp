495e4790734cae8ff95ea6f946014ebb9ab32ad5
Dawid Dylus <36481644+DawidDylus@users.noreply.github.com>
2021-07-27 14:23:27 +0200

    Creating structure to access information in database via CRUD operations (service, repository)

--- src/main/java/com/students/management/StudentManagementApp/student/StudentController.java
+++ src/main/java/com/students/management/StudentManagementApp/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.students.management.StudentManagementApp.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents(){
-        List<Student> students = Arrays.asList(
-                new Student(1L, "Patrycja", "patrycja@gmail.com", Gender.FEMALE),
-                new Student(2L, "Adam", "adam@gmail.com", Gender.MALE)
-        );
-        return students;
+        return studentService.getAllStudent();
     }
 
 }
